0b8fc1218b4021ac06ee63833a042c09b399d57f
Kariuki11 <irungukenneth433@gmail.com>
2024-07-17 18:42:04 +0300

    New changes

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 
 
-
 const TodoList = () => {
+const [todo, setTodo] = useState("");
 const [todos, setTodos] = useState([
     'clean Utensils',
     'clean the house',
     'clean the car',
 ])
 
-  
+const handleSubmit = () => {
+    setTodos((prev) => [...prev, todo])
+}
+
   return (
     <>
-    <form>
+    <form onSubmit={handleSubmit}>
         <input placeholder="Add item" value={todo} onChange={(e) => setTodo(e.target.value)}/>
+        <button type="submit">Add Item</button>
     </form>
     <ul>
         {todos.map((item, index) => (
